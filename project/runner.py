282ad0c0b093dd7447e6c7bb262ec2cb96ba0f44
gofiny <gofiny@inbox.ru>
2020-09-27 19:14:26 +0700

    add webhook app

---
+++ project/runner.py
@@ -0,0 +1,16 @@
+import argparse
+from web_app import WebApp
+from config import DEBUG_SECRET_STR, DEBUG_RETURING_CALLBACK_STR
+
+
+if __name__ == "__main__":
+    parser = argparse.ArgumentParser()
+    parser.add_argument("--path")
+    args = parser.parse_args()
+    app = WebApp(
+        address_prefix="my_gang",
+        secret_str=DEBUG_SECRET_STR,
+        returning_callback_str=DEBUG_RETURING_CALLBACK_STR
+    )
+
+    app.start_app(socket_path=args)
--- project/web_app.py
+++ project/web_app.py
@@ -1,11 +1,25 @@
-from aiohttp import web
+from aiohttp.web import Application, Response, Request, post, run_app
+from typing import Optional
 
 
-async def test(request):
-    return web.Response(text="ok")
+class WebApp:
+    def __init__(self, address_prefix: str,  secret_str: str, returning_callback_str: str):
+        self.secret_str = secret_str
+        self.app = Application()
+        self.returning_callback_str = returning_callback_str
+        self._set_base_handler(address_prefix=address_prefix)
 
+    def _set_base_handler(self, address_prefix: str):
+        self.app.add_routes([post(f"{address_prefix}/", self._base_handler)])
 
-app = web.Application()
-app.add_routes([web.get("/", test)])
+    async def _base_handler(self, request: Request) -> Response:
+        request_json = await request.json()
+        if request_json.get("secret") != self.secret_str:
+            return Response(status=404)
+        if request_json.get("type") == "confirmation":
+            return Response(body=self.returning_callback_str)
 
-web.run_app(app)
+        return Response(body="ok")
+
+    def start_app(self, socket_path=None):
+        run_app(self.app, path=socket_path)
