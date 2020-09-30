1d37ebe743260a5b1300557076e57f492f93a27b
gofiny <gofiny@inbox.ru>
2020-09-30 21:13:22 +0700

    test

--- project/db_utils/redis_queries.py
+++ project/db_utils/redis_queries.py
@@ -5,8 +5,7 @@ from db_utils.models import User
 async def return_or_create_user(pool: Redis, user_id: int) -> User:
     is_exist = await pool.exists(key=f"users:{user_id}")
     if is_exist:
-        user = await pool.hgetall(key=f"users:{user_id}", encoding=dict)
-        print(user)
+        user = await pool.hgetall(key=f"users:{user_id}", encoding="utf-8")
     else:
         user = {"user_id": user_id, "if_followed": 0}
         await pool.hmset_dict(f"users:{user['user_id']}", user)
