7e6b93857f530dc13b79fb7493fe572f72808dec
gofiny <gofiny@inbox.ru>
2020-10-02 21:16:51 +0700

    fix

--- project/db_utils/pg_queries.py
+++ project/db_utils/pg_queries.py
@@ -8,7 +8,7 @@ def transaction(func):
         params = {**kwargs}
         connection = params["connection"]
         async with connection.transaction():
-            result = await func(**params)
+            result = await func(*args, **kwargs)
         return result
     return wrapper
 
@@ -20,4 +20,4 @@ async def preparing_db(connection: Connection):
 
 @transaction
 async def get_user(connection: Connection, user_id: int) -> Optional[Record]:
-    return await connection.fetchrow(sql.select_user_by_user_id % user_id)
+    return await connection.fetchrow(sql.select_user_by_user_id, user_id)
