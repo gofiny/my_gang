0c807b967ea9f3d88e81f0b69511e0b406f63e9b
gofiny <gofiny@inbox.ru>
2020-10-03 20:45:17 +0700

    make beauty buttons

--- project/bot_utils/keyboards.py
+++ project/bot_utils/keyboards.py
@@ -3,19 +3,21 @@ from vk_api.vk import Keyboard, Button
 
 def subscribe() -> Keyboard:
     keyboard = Keyboard()
-    keyboard.add_button(Button(label="Новости"))
-    keyboard.add_button(Button(label="Подписаться на рассылку", payload={"command": "subscribe"}))
+    keyboard.add_button(Button(label="Новости", color="primary"))
+    keyboard.add_empty_row()
+    keyboard.add_button(Button(label="Подписаться на рассылку", payload={"command": "subscribe"}, color="positive"))
     return keyboard
 
 
 def unsubscribe() -> Keyboard:
     keyboard = Keyboard()
-    keyboard.add_button(Button(label="Новости"))
-    keyboard.add_button(Button(label="Отписаться от рассылки", payload={"command": "unsubscribe"}))
+    keyboard.add_button(Button(label="Новости", color="primary"))
+    keyboard.add_empty_row()
+    keyboard.add_button(Button(label="Отписаться от рассылки", payload={"command": "unsubscribe"}, color="negative"))
     return keyboard
 
 
 def telegram_link() -> Keyboard:
     keyboard = Keyboard(inline=True)
-    keyboard.add_button(Button(b_type="open_link", label="Новости", link="https://t.me/MyGangNews"))
+    keyboard.add_button(Button(b_type="open_link", label="Перейти в наш telegram канал", link="https://t.me/MyGangNews"))
     return keyboard
