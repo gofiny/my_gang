499153cd158d7cf2df90df28f8b2bf1028ae299c
gofiny <gofiny@inbox.ru>
2020-10-03 21:15:29 +0700

    fix

--- project/db_utils/models.py
+++ project/db_utils/models.py
@@ -1,4 +1,4 @@
 class User:
     def __init__(self, data: dict):
-        self.user_id = data.get("user_id")
+        self.user_id = int(data.get("user_id"))
         self.is_followed = data.get("is_followed")
