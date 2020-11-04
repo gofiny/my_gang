701f0c764f106e785a27564edf18ea5f86cce7c4
gofiny <gofiny@inbox.ru>
2020-11-04 13:18:34 +0700

    test

--- project/db_utils/redis_queries.py
+++ project/db_utils/redis_queries.py
@@ -10,7 +10,7 @@ async def add_player(pool: Redis, player: Player) -> None:
 
 async def get_await_fight(pool: Redis) -> Optional[str]:
     fight = await pool.getset(key="fight", value="", encoding="utf-8")
-    logger.debug(f"{fight}")
+    logger.info(f"{fight}")
     if fight == "":
         return None
     return fight
